cc486d8fa3a7116949ba81be27070879b076592c
Puplishe <dimaivanoov1991@gmail.com>
2023-11-26 07:22:03 +0300

    Добавлен функционал фильтрации книг и поиска книг

--- store/book/views.py
+++ store/book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import permissions, viewsets
+from rest_framework import filters, viewsets
 from rest_framework.permissions import AllowAny
 from .models import Book
 from .serializers import BookSerializer
@@ -8,4 +8,6 @@ class BookListApiView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AllowAny,]
-
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    search_fields = ['author', 'publish_year', 'name']
+    
